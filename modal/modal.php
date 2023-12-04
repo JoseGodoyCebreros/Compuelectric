7eb25ae9b262cf7e47a993fe8bb3b9008b8d46ac
José Godoy Cebreros <110354473+JoseGodoyCebreros@users.noreply.github.com>
2023-12-04 12:43:42 -0500

    Add files via upload

--- modal/modal.php
+++ modal/modal.php
@@ -8,15 +8,15 @@
 <!-- CSS MODALES -->
 <div id="modales-container">
     <?php 
-    $registros = array_merge($porteros, $intercom, $placas, $vportero, $robotica);
+    $registros = array_merge($porteros, $intercom, $placas, $vportero, $camaras, $robotica);
 
     foreach($registros as $registro) {
     ?>    
         <div class="contenedor-modal" id="<?php echo $registro['id_modal']?>" role="document">
             <div class="modal-principal" role="document">
-                <div class="modal--header">
+                <div class="modal--header"> 
                     <div class="modal--header-container">
-                        <p class="modal-title"> <?php echo $registro['nombrePortero'] ?? $registro['nombreCompleto'] ?? $registro['nombreCompletoPlaca'] ?? $registro['nomCom_vportero'] ?? $registro['nomRobotica']?></p>
+                        <p class="modal-title"> <?php echo $registro['nombrePortero'] ?? $registro['nombreCompleto'] ?? $registro['nombreCompletoPlaca'] ?? $registro['nomCom_vportero'] ?? $registro['nombreCamara'] ?? $registro['nomRobotica']?></p>
                         <button class="botonModal-cerrar" onclick="cerrarModal('<?php echo $registro['id_modal']?>')">
                             <span aria-hidden="true">X</span>
                         </button>
@@ -37,7 +37,7 @@
                 <div class="modal-footer">
                     <p class="precio"><?php echo $registro['precio']?></p>
                     <a class="btn btn-primary" target="_blank" href="https://api.whatsapp.com/send?phone=977679527&text=Buenas%20tardes,%20quisiera%20preguntar%20acerca%20del%20
-                    <?php echo $registro['nombrePortero'] ?? $registro['nombreCompleto'] ?? $registro['nombreCompletoPlaca'] ?? $registro['nomCom_vportero'] ?? $registro['nomRobotica']?>:">PEDIR</a>
+                    <?php echo $registro['nombrePortero'] ?? $registro['nombreCompleto'] ?? $registro['nombreCompletoPlaca'] ?? $registro['nomCom_vportero'] ?? $registro['nombreCamara'] ?? $registro['nomRobotica']?>:">PEDIR</a>
                 </div>
             </div>
         </div>
